4f42c0f8935a72788d0c8667d99c772cbaf91786
Stephen Blais <stephen@s-blais.com>
2019-10-05 15:20:01 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author INTEGER,
+  sub_genre INTEGER
+);
+
+CREATE TABLE sub_genres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  year INTEGER,
+  series INTEGER
+);
+
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author INTEGER
+);
