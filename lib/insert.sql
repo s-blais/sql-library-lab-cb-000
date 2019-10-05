2f34ec5c336661441a7eda8d5599d34a4655d346
Stephen Blais <stephen@s-blais.com>
2019-10-05 16:02:51 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -38,3 +38,19 @@ INSERT INTO characters (name, species, motto, author_id)
   VALUES ("Bailey", "Cockapoo", "I'm going to pee everywhere", 2);
 INSERT INTO characters (name, species, motto, author_id)
   VALUES ("Bug", "Pit Bull", "I''m a gentle lady", 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (1, 3);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 2);
+INSERT INTO character_books (character_id, book_id) VALUES (2, 3);
+INSERT INTO character_books (character_id, book_id) VALUES (3, 1);
+INSERT INTO character_books (character_id, book_id) VALUES (4, 3);
+INSERT INTO character_books (character_id, book_id) VALUES (5, 4);
+INSERT INTO character_books (character_id, book_id) VALUES (6, 5);
+INSERT INTO character_books (character_id, book_id) VALUES (7, 4);
+INSERT INTO character_books (character_id, book_id) VALUES (7, 5);
+INSERT INTO character_books (character_id, book_id) VALUES (7, 6);
+INSERT INTO character_books (character_id, book_id) VALUES (8, 4);
+INSERT INTO character_books (character_id, book_id) VALUES (8, 5);
+INSERT INTO character_books (character_id, book_id) VALUES (8, 6);
