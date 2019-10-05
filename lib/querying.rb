980ad5741d0520cf2e381871d10d6dddc5b37b24
Stephen Blais <stephen@s-blais.com>
2019-10-05 16:28:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species)
   FROM characters
-  GROUP BY characters.species LIMIT 1;"
+  GROUP BY characters.species LIMIT 1 DESC;"
 end
 
 def select_name_and_series_subgenres_of_authors
