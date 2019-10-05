21e757904223efb8c136e521efbedf468fcb5d3a
Stephen Blais <stephen@s-blais.com>
2019-10-05 16:18:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,7 +6,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters
-  ORDER BY characters.motto DESC
+  ORDER BY CHAR_LENGTH(characters.motto)
   LIMIT 1;"
 end
 
